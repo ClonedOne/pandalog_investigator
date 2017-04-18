76ebe95a689105ce7ca31b87709a4c9e39db9efc
ClonedOne <severi.giorgio@gmail.com>
2017-04-18 17:46:26 +0200

    adding learning initial structure

---
+++ pandaloginvestigator/cli/cmds/learn.py
@@ -0,0 +1,69 @@
+from pandaloginvestigator.core.learn import log_learner
+from pandaloginvestigator.core.utils import string_utils
+import logging
+import os
+
+
+logger = logging.getLogger(__name__)
+
+
+def learn_command(app):
+    try:
+        dir_pandalogs_path = app.config.get('pandaloginvestigator', 'dir_pandalogs_path')
+    except:
+        logger.error('dir_pandalogs_path not set in configuration file')
+        return
+    try:
+        dir_database_path = app.config.get('pandaloginvestigator', 'dir_database_path')
+    except:
+        logger.error('dir_database_path not set in configuration file')
+        return
+    try:
+        core_num = app.config.get('pandaloginvestigator', 'core_num')
+    except:
+        logger.error('core_num not set in configuration file')
+        return
+    try:
+        created_dirs_path = app.config.get('pandaloginvestigator', 'created_dirs_path')
+    except:
+        logger.error('created_dirs_path not set in configuration file')
+        return
+    try:
+        dir_panda_path = app.config.get('pandaloginvestigator', 'dir_panda_path')
+    except:
+        logger.error('dir_panda_path not set in configuration file')
+        return
+
+    dir_unpacked_path = os.path.join(created_dirs_path, string_utils.dir_unpacked_path)
+    if not os.path.exists(dir_unpacked_path):
+        os.makedirs(dir_unpacked_path)
+
+    dir_analyzed_path = os.path.join(created_dirs_path, string_utils.dir_analyzed_path)
+    if not os.path.exists(dir_analyzed_path):
+        os.makedirs(dir_analyzed_path)
+
+    dir_results_path = os.path.join(created_dirs_path, string_utils.dir_results_path)
+    if not os.path.exists(dir_results_path):
+        os.makedirs(dir_results_path)
+
+    logger.debug(
+        'Learning command with parameters: {}, {}, {}, {}, {}, {}, {}'.format(
+            dir_panda_path,
+            dir_pandalogs_path,
+            dir_unpacked_path,
+            dir_analyzed_path,
+            dir_results_path,
+            dir_database_path,
+            str(core_num)
+        )
+    )
+
+    log_learner.learn_logs(
+        dir_panda_path,
+        dir_pandalogs_path,
+        dir_unpacked_path,
+        dir_analyzed_path,
+        dir_results_path,
+        dir_database_path,
+        core_num
+    )
