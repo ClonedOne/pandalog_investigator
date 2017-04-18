5e850abc941de87858b89a98d932ab65d6fcee15
ClonedOne <severi.giorgio@gmail.com>
2017-04-18 17:46:26 +0200

    adding learning initial structure

---
+++ pandaloginvestigator/core/learn/log_learner.py
@@ -0,0 +1,21 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def learn_logs(dir_panda_path, dir_pandalogs_path, dir_unpacked_path, dir_analyzed_path, dir_results_path,
+               dir_database_path, core_num):
+    """
+    Learns the structure of the analyzed pandalog files.
+    
+    :param dir_panda_path: path to the pandalog unpacker tool
+    :param dir_pandalogs_path: path to the pandalogs
+    :param dir_unpacked_path: path to the unpacked pandalogs
+    :param dir_analyzed_path: path to the analysis results
+    :param dir_results_path: path to the global result folder
+    :param dir_database_path: path to the database
+    :param core_num: number of available CPU cores (as per configuration)
+    :return: 
+    """
+
+    pass
